4eabbd95531275be7141e82a028ea7bc7e14693b
djpole <djpole@hotmail.com>
2025-04-26 10:56:35 +0200

    Add files via upload
    Adds Spanish translation to node

---
+++ languages/es.js
@@ -0,0 +1,38 @@
+module.exports = {
+    descriptions: {
+        "N":     "Norte",
+        "NNE":   "Norte-noreste",
+        "NE":    "Noreste",
+        "ENE":   "Este-noreste",
+        "E":     "Este",
+        "ESE":   "Este-sureste",
+        "SE":    "Sureste",
+        "SSE":   "Sur-sureste",
+        "S":     "Sur",
+        "SSW":   "Sur-suroeste",
+        "SW":    "Suroeste",
+        "WSW":   "Oeste-suroeste",
+        "W":     "Oeste",
+        "WNW":   "Oeste-noroeste",
+        "NW":    "Noroeste",
+        "NNW":   "Norte-noroeste"
+    },
+    abbreviations: {
+        "N":     "N",
+        "NNE":   "NNE",
+        "NE":    "NE",
+        "ENE":   "ENE",
+        "E":     "E",
+        "ESE":   "ESE",
+        "SE":    "SE",
+        "SSE":   "SSE",
+        "S":     "S",
+        "SSW":   "SSO",
+        "SW":    "SO",
+        "WSW":   "OSO",
+        "W":     "O",
+        "WNW":   "ONO",
+        "NW":    "NO",
+        "NNW":   "NNO"
+     }
+}
